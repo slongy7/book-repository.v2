0297604b04b29cee7e96eefb0fff5cc95a21f726
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-10-14 14:01:35 -0400

    utils update

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,41 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $passsword: String!) {
+        login(email: $email, password: $passsword) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }`;
+
+    export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }`;
+
+    export const SAVE_BOOK = gql`
+        mutation saveBook($bookAuthor: [bookAuthor], $description: String!, $title: String, $bookId: String, $image: String) {
+            saveBook(bookAuthor: $bookAuthor, description: $description, $title: $title, $bookId: bookId, $image: image) {
+            _id
+            name
+            savedBooks
+        }
+    }`;
+
+    export const REMOVE_BOOK = gql`
+        mutation removeBook($book: String!) {
+            removeBook(book: $book) {
+                _id
+                name
+                savedBooks
+            }
+        }`;
